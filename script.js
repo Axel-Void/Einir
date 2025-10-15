526c5aab99b5a0b8758a5840e4364820a461306d
Axel-Void <kiamasarifu@gmail.com>
2025-10-15 11:22:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function addTask () {
+  const inputField = document.getElementById("task-input");
+  const listContainer = document.getElementById("task-list");
+
+  let taskText = inputField.value.trim();
+
+  if (taskText === "") {
+    alert("Please Add Task!!!");
+    return;
+
+  const listItem = document.createElement("li");
+
+  listItem.innerHTML = taskText;
   
+  listContainer.appendChild(listItem);
+
+  inputField.value = "";
   }
+}
   
